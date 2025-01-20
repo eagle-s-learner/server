bf62d25a6ccda9cf302355152b012b83a4e4f9f7
satyam kr <satyam.9242@gmail.com>
2025-01-20 20:13:59 +0530

    changed in cors middleware

--- index.js
+++ index.js
@@ -17,11 +17,12 @@ const cartItemQuantiyUpdate = require("./routes/cartItemQuantiyUpdate.js");
 dotenv.config();
 
 const app = express();
+const origin = process.env.CLIENT;
 
 app.use(bodyParser.json());
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT,
+    origin: origin,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
 app.use(cookieParser());
